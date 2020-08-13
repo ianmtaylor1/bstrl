8f62d07892c5180a9d3cd84ee62deb90edaca82e
Ian Taylor <ian.taylor@colostate.edu>
2020-08-13 11:25:13 -0600

    Updated tripartite sampler to allow permuted or resampled pprb proposals

--- R/tripartite.R
+++ R/tripartite.R
@@ -7,48 +7,71 @@
 #    at end of this function?
 #' @export
 tripartiteRL.precmp <- function(cmpdata.1to2, cmpdata.1to3, cmpdata.2to3, trace=FALSE,
-                                nIter=1000, burn=round(nIter*.1), a=1, b=1, aBM=1, bBM=1, seed=0) {
+                                nIter.bi=1200, burn.bi=round(nIter.bi*.1),
+                                nIter.tri=nIter.bi-burn.bi, burn.tri=round(nIter.tri*.1),
+                                pprb.method="ordered",
+                                a=1, b=1, aBM=1, bBM=1, seed=0) {
+  # Parameter checking
+  if (! is.element(pprb.method, c("ordered","permuted","resampled"))) {
+    stop("pprb.method must be one of 'ordered', 'permuted', or 'resampled'")
+  }
+  if (is.element(pprb.method, c("ordered","permuted")) && (nIter.tri != nIter.bi - burn.bi)) {
+    stop("If pprb.method is 'ordered' or 'permuted', require nIter.tri == nIter.bi - burn.bi")
+  }
+
   # 1. Size of files
   n1 <- cmpdata.1to3$n1
   n2 <- cmpdata.2to3$n1
   n3 <- cmpdata.1to3$n2
   # 2. Do bipartite RL between df1 and df2 using BRL.gibbs()
   #    Do not burn anything now! Keep it all, and burn later
-  bipartite.samp <- BRL.gibbs.precmp(cmpdata.1to2, nIter, 0, a, b, aBM, bBM, seed)
+  bipartite.samp <- BRL.gibbs.precmp(cmpdata.1to2, nIter.bi, burn.bi, a, b, aBM, bBM, seed)
   # 3. Create precomputed data structures
   comparisons.1to3 <- preproc.cmpdata(cmpdata.1to3)
   comparisons.2to3 <- preproc.cmpdata(cmpdata.2to3)
   # 4. Set up empty arrays of the appropriate size that will eventually be returned
-  m.samples  <- matrix(0, nrow=nrow(bipartite.samp$m), ncol=nIter)
-  u.samples  <- matrix(0, nrow=nrow(bipartite.samp$u), ncol=nIter)
-  Z.samples  <- matrix(0, nrow=nrow(bipartite.samp$Z), ncol=nIter)
-  Z2.samples <- matrix(0, nrow=n3,                     ncol=nIter)
-  # 5. Initialize the first sample (m,u,Z: first sample from bipartite. Z2: unlinked)
-  m.samples[,1] <- bipartite.samp$m[,1]
-  u.samples[,1] <- bipartite.samp$u[,1]
-  Z.samples[,1] <- bipartite.samp$Z[,1]
+  m.samples  <- matrix(0, nrow=nrow(bipartite.samp$m), ncol=nIter.tri)
+  u.samples  <- matrix(0, nrow=nrow(bipartite.samp$u), ncol=nIter.tri)
+  Z.samples  <- matrix(0, nrow=nrow(bipartite.samp$Z), ncol=nIter.tri)
+  Z2.samples <- matrix(0, nrow=n3,                     ncol=nIter.tri)
+  # 5. Based on whether the PPRB is ordered, permuted, or resampled, produce a
+  #    list of the proposal indexes
+  if (pprb.method == "ordered") {
+    pprb.index <- seq_len(nIter.tri)
+  } else if (pprb.method == "permuted") {
+    pprb.index <- sample(nIter.tri)
+  } else if (pprb.method == "resampled") {
+    pprb.index <- sample(nIter.bi-burn.bi, size=nIter.tri, replace=TRUE)
+  }
+  # 6. Initialize the first sample (m,u,Z: first sample from bipartite. Z2: unlinked)
+  m.samples[,1] <- bipartite.samp$m[,pprb.index[1]]
+  u.samples[,1] <- bipartite.samp$u[,pprb.index[1]]
+  Z.samples[,1] <- bipartite.samp$Z[,pprb.index[1]]
   Z2.samples[,1] <- n1 + n2 + seq_len(n3)
   # Create array to hold record of acceptances: first row = m,u,Z. second row = Z2
   # Contains all 1's by default, zeros written on rejection
-  accepted <- matrix(1, nrow=2, ncol=nIter)
+  accepted <- matrix(1, nrow=2, ncol=nIter.tri)
+  # Creates array to track impossible proposals: proposals of m,u,Z from pprb
+  # which have probability zero
+  pprb.impossible <- rep(0, nIter.tri)
   # Choose the likelihood function based on tracing flag
   if (trace) {
     ell <- calc.log.lkl.tracing
   } else {
     ell <- calc.log.lkl
   }
-  # 6. Perform M-H for each iteration
-  for (i in seq(2, nIter)) {
+  # 7. Perform M-H for each iteration
+  for (i in seq(2, nIter.tri)) {
     # What are the current values of the parameters?
     m.curr <- m.samples[,i-1]
     u.curr <- u.samples[,i-1]
     Z.curr <- Z.samples[,i-1]
     Z2.curr <- Z2.samples[,i-1]
-    # 6.1: Propose new m,u,Z collectively. Take sequential samples from bipartite
+    # 7.1: Propose new m,u,Z collectively. Take sequential samples from bipartite
     # TODO Future: resample with replacement from bipartite posterior samples?
-    m.prop <- bipartite.samp$m[,i]
-    u.prop <- bipartite.samp$u[,i]
-    Z.prop <- bipartite.samp$Z[,i]
+    m.prop <- bipartite.samp$m[,pprb.index[i]]
+    u.prop <- bipartite.samp$u[,pprb.index[i]]
+    Z.prop <- bipartite.samp$Z[,pprb.index[i]]
     # Decide whether to accept new values
     log.alpha1 <- (
       ell(comparisons.1to3, comparisons.2to3, n1, n2, n3, m.prop, u.prop, Z.prop, Z2.curr)
@@ -72,7 +95,7 @@ tripartiteRL.precmp <- function(cmpdata.1to2, cmpdata.1to3, cmpdata.2to3, trace=
     m.curr <- m.samples[,i]
     u.curr <- u.samples[,i]
     Z.curr <- Z.samples[,i]
-    # 6.2: Propose new Z2
+    # 7.2: Propose new Z2
     tmp <- draw.Z2.informed(n1, n2, n3, Z.curr, Z2.curr, m.curr, u.curr,
                             comparisons.1to3, comparisons.2to3,
                             aBM, bBM, trace=trace)
@@ -92,20 +115,24 @@ tripartiteRL.precmp <- function(cmpdata.1to2, cmpdata.1to3, cmpdata.2to3, trace=
       # reject
       Z2.samples[,i] <- Z2.curr
       accepted[2,i] <- 0
+      if (log.alpha2 == -Inf) {
+        pprb.impossible[i] <- 1
+      }
     }
     # Print updates
-    if (i  %% max(nIter %/% 20, 1) == 0) {
+    if (i  %% max(nIter.tri %/% 20, 1) == 0) {
       cat("Iteration ", i, "\n")
     }
   }
-  # 7. Burn the initial samples from both the bipartite and tripartite links, and...
-  keptsamples <- setdiff(1:nIter,seq_len(burn))
-  # 8. Return new list comprised of only accepted samples and new matchings, Z2
+  # 8. Burn the initial samples from both the bipartite and tripartite links, and...
+  keptsamples <- setdiff(1:nIter.tri,seq_len(burn.tri))
+  # 9. Return new list comprised of only accepted samples and new matchings, Z2
   return(list(Z1=Z.samples[,keptsamples,drop=FALSE],
               Z2=Z2.samples[,keptsamples,drop=FALSE],
               m=m.samples[,keptsamples,drop=FALSE],
               u=u.samples[,keptsamples,drop=FALSE],
-              accepted=accepted[,keptsamples,drop=FALSE]))
+              accepted=accepted[,keptsamples,drop=FALSE],
+              pprb.impossible=pprb.impossible[keptsamples]))
 }
 
 #' @export
@@ -113,7 +140,9 @@ tripartiteRL <- function(df1, df2, df3,
                          flds=NULL, flds1=NULL, flds2=NULL, flds3=NULL,
                          types=NULL, breaks=c(0,.25,.5),
                          trace=FALSE,
-                         nIter=1000, burn=round(nIter*.1), a=1, b=1, aBM=1, bBM=1, seed=0) {
+                         nIter.bi=1200, burn.bi=round(nIter.bi*.1),
+                         nIter.tri=nIter.bi-burn.bi, burn.tri=round(nIter.tri*.1),
+                         a=1, b=1, aBM=1, bBM=1, seed=0) {
   # Create comparison data using BRL::compareRecords
   cmpdata.1to2 <- compareRecords(df1, df2, flds, flds1=flds1, flds2=flds2,
                                  types=types, breaks=breaks)
@@ -123,7 +152,8 @@ tripartiteRL <- function(df1, df2, df3,
                                  types=types, breaks=breaks)
   # Call function with precompared files
   tripartiteRL.precmp(cmpdata.1to2, cmpdata.1to3, cmpdata.2to3, trace,
-                      nIter, burn, a, b, aBM, bBM, seed)
+                      nIter.bi, burn.bi, nIter.tri, burn.tri,
+                      a, b, aBM, bBM, seed)
 }
 
 
