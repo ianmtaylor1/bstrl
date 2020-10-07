856913cd1a548741649f7776a436e1d42c044960
Ian Taylor <ian.taylor@colostate.edu>
2020-10-07 12:38:05 -0600

    Added r_m_u_fc sampling function.

--- R/m_u_fc.R
+++ R/m_u_fc.R
@@ -4,6 +4,46 @@
 # It depends on the package extraDistr for calculating the density and sampling
 # from Dirichlet distributions
 
+# Draw m and u from their full conditional distributions.
+# Parameters:
+#   cmpdata.1to2, cmpdata.1to3 - comparisons with the "new" third file.
+#   Z, Z2 - current values of the parameters Z and Z2
+#   a, b - prior hyperparameters for the distributions of m and u, respectively.
+#          See ?BRL::BRL for explanation
+#   m.prev.pars - additions to hyperparameters for m from matches between
+#          previous files. These are computed in a post-processing step of the
+#          samples of Z, and can just be passed directly here.
+#   u.prev.pars - same as m.prev.pars, but for u.
+#   trace - whether to do link tracing.
+# Return:
+#   A list with two elements, m and u, which contain the fc sampled m and u.
+r_m_u_fc <- function(cmpdata.1to3, cmpdata.2to3, Z, Z2,
+                     a, b, m.prev.pars, u.prev.pars, trace=FALSE) {
+  # 1. Create the additions to m and u hyperparameters derived from links with
+  # the newest (third) file.
+  matchrows.2to3 <- matchrows(cmpdata.2to3, Z2, offset=cmpdata.1to3$n1)
+  if (trace) {
+    matchrows.1to3 <- matchrows(cmpdata.1to3,
+                                trace(Z2, Z, steps=1, offset=cmpdata.1to3$n1),
+                                offset=0)
+    noncandrows.1to3 <- c()
+  } else {
+    matchrows.1to3 <- matchrows(cmpdata.1to3, Z2, offset=0)
+    noncandrows.1to3 <- noncandrows(cmpdata.1to3, Z, offset=0)
+  }
+  m.new.pars <- colSums(cmpdata.1to3$comparisons[matchrows.1to3,,drop=FALSE]) +
+    colSums(cmpdata.2to3$comparisons[matchrows.2to3,,drop=FALSE])
+  u.new.pars <- attr(cmpdata.1to3$comparisons, "totals") +
+    attr(cmpdata.2to3$comparisons, "totals") -
+    m.new.pars -
+    colSums(cmpdata.1to3$comparisons[noncandrows.1to3,,drop=FALSE])
+  # 2. combine a, m.prev.pars and new m pars to sample m
+  m <- rdirichlet.multi(alpha=m.new.pars + m.prev.pars + a, groups=cmpdata.1to3$nDisagLevs)
+  # 3. combine b, u.prev.pars and new u pars to sample u
+  u <- rdirichlet.multi(alpha=u.new.pars + u.prev.pars + b, groups=cmpdata.1to3$nDisagLevs)
+  # 4. Put into list and return
+  return(list(m=m, u=u))
+}
 
 ################################################################################
 ## HELPER FUNCTIONS ############################################################
