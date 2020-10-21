2fb3d35854f4909916c5a61c24605c80077afa9e
Ian Taylor <ian.taylor@colostate.edu>
2020-10-21 14:24:12 -0600

    Added bipartite option for locally balanced proposals

--- R/bipartite.R
+++ R/bipartite.R
@@ -17,14 +17,54 @@
 #     conditional of m given Z and the comparisons.
 #   u.fc.pars - Same as ^ but for u.
 #' @export
-bipartiteRL.precmp <- function(cmpdata, nIter=1000, burn=round(nIter*.1), a=1, b=1, aBM=1, bBM=1, seed=0) {
+bipartiteRL.precmp <- function(cmpdata, nIter=1000, burn=round(nIter*.1), a=1, b=1, aBM=1, bBM=1,
+                               seed=0, method="BRL", blocksize=NULL) {
 
   # 'burn' is not a parameter of the other functions, so we control it here
   if( !is.numeric(burn) | (burn<0) | (burn>=nIter) )
     stop("burn should be an integer that satisfies 0 <= burn < nIter")
 
   # Gibbs sampling from posterior of bipartite matchings
-  chain <- BRL::bipartiteGibbs(cmpdata, nIter, a, b, aBM, bBM, seed)
+  if (method == "BRL") {
+    chain <- BRL::bipartiteGibbs(cmpdata, nIter, a, b, aBM, bBM, seed)
+  } else if (method == "LB") {
+    # Draw initial values for m and u from the prior
+    if (length(a) == 1) a <- rep(a, sum(cmpdata$nDisagLevs))
+    if (length(b) == 1) b <- rep(b, sum(cmpdata$nDisagLevs))
+    m.curr <- rdirichlet.multi(a, cmpdata$nDisagLevs)
+    u.curr <- rdirichlet.multi(b, cmpdata$nDisagLevs)
+    # Initial Z is all unlinked
+    Z.curr <- cmpdata$n1 + seq_len(cmpdata$n2)
+    # Initialize arrays to hold samples
+    m.samples <- u.samples <- matrix(0, nrow=sum(cmpdata$nDisagLevs), ncol=nIter)
+    Z.samples <- matrix(0, nrow=cmpdata$n2, ncol=nIter)
+    # Iterate through sampler
+    for (i in 1:nIter) {
+      # 1. Full conditional update of Z conditioned on m and u.
+      tmp <- draw.Z2.informed(list(cmpdata), c(), Z.curr, m, u, aBM, bBM, blocksize=blocksize)
+      Z.prop <- tmp$Z2
+      mhmod <- tmp$mod
+      log.alpha <- (
+        calc.log.lkl(list(cmpdata), m.curr, u.curr, c(), Z.prop)
+        - calc.log.lkl(list(cmpdata), m.curr, u.curr, c(), Z.curr)
+        + calc.log.Z2prior(cmpdata$n1, Z.prop, c(), aBM, bBM)
+        - calc.log.Z2prior(cmpdata$n1, Z.curr, c(), aBM, bBM)
+        + log(mhmod)
+      )
+      if (-rexp(1) < log.alpha) {
+        Z.curr <- Z.prop
+      }
+      Z.samples[,i] <- Z.curr
+      # 2. Full conditional update of m and u conditioned on Z.
+      tmp <- r_m_u_fc(list(cmpdata), c(), Z.curr, a, b, 0, 0)
+      m.curr <- m.samples[,i] <- tmp$m
+      u.curr <- u.samples[,i] <- tmp$u
+    }
+    # Put into same format as BRL
+    chain <- list(m=m.samples, u=u.samples, Z=Z.samples)
+  } else {
+    stop(paste0("Method '", method, "' unknown."))
+  }
 
   # Calculate comparison summaries for each value of Z
   # At bipartite stage, there is only one cmpdata object and tracing is irrelevant
