9ffa5acf4a8dc8bcc1c12a9fa78e99836765c99f
Ian Taylor <ian.taylor@colostate.edu>
2021-05-11 15:54:27 -0600

    Added export to preprocessing comparison data function for use with debugging.

--- R/sampler_preprocess.R
+++ R/sampler_preprocess.R
@@ -5,6 +5,7 @@
 # This function takes an object comparing two files, as returned by
 # BRL::compareRecords, and preprocesses it into a form that is most useful to
 # the bstrl sampler.
+#' @export
 preproc.cmpdata <- function(compared) {
   # Attach the column sums as an attribute. Column sums are needed in likelihood
   # functions, and computing them over and over again adds up in terms of comp
