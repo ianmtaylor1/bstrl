a1d99f1a8ce9af8dac00dbc503936fce2845177d
Ian Taylor <ian.taylor@colostate.edu>
2022-01-26 22:19:39 -0700

    Updated bipartite linkage to store summary statistics of previous comparisons

--- R/bipartite.R
+++ R/bipartite.R
@@ -140,16 +140,38 @@ bipartiteRL <- function(df1, df2,
   chain <- BRL::bipartiteGibbs(cmpdata, nIter, a, b, aBM, bBM, seed)
 
   # Store files and comparison data in format we will use later
-  chain$files <- list(df1, df2)
-  chain$comparisons <- list(list(cmpdata))
+  files <- list(df1, df2)
+  comparisons <- list(list(cmpdata))
 
   # Store comparison and prior parameters
-  chain$priors <- list(a=a, b=b, aBM=aBM, bBM=bBM)
-  chain$cmpdetails <- list(fldsglobal=flds, types=types, breaks=breaks,
-                           flds=list(flds1, flds2))
+  priors <- list(a=a, b=b, aBM=aBM, bBM=bBM)
+  cmpdetails <- list(fldsglobal=flds, types=types, breaks=breaks,
+                     flds=list(flds1, flds2))
 
-  attr(chain, "class") <- "bstrlstate"
+  # Store summary statistics of comparisons for use later
+  m.fc.pars <- matrix(0, nrow=nrow(chain$m), ncol=nIter)
+  u.fc.pars <- matrix(0, nrow=nrow(chain$m), ncol=nIter)
+  for (i in 1:nIter) {
+    tmp <- disag.counts.lastfile(list(cmpdata), streaminglinks(c(nrow(df1), nrow(df2)), chain$Z[,i]))
+    m.fc.pars[,i] <- tmp$match
+    u.fc.pars[,i] <- tmp$nonmatch
+  }
 
-  return(chain)
+  # Burn and return complete structure
+  iterfilter <- setdiff(seq_len(nIter), seq_len(burn))
+  structure(
+    list(
+      Z = chain$Z[,iterfilter,drop=F],
+      m = chain$m[,iterfilter,drop=F],
+      u = chain$u[,iterfilter,drop=F],
+      files = files,
+      comparisons = comparisons,
+      cmpdetails = cmpdetails,
+      priors = priors,
+      m.fc.pars = m.fc.pars[,iterfilter,drop=F],
+      u.fc.pars = u.fc.pars[,iterfilter,drop=F]
+    ),
+    class = "bstrlstate"
+  )
 }
 
