7ba68f852c60f54726e9bf0a85cf3721aa4380dd
Ian Taylor <ian.taylor@colostate.edu>
2022-02-02 23:58:58 -0700

    Stubbed SMCMC update function
    With filled in locally balanced jumping kernel and m,u full conditional for transition kernel

--- R/SMCMCupdate.R
+++ R/SMCMCupdate.R
@@ -13,12 +13,12 @@ SMCMCupdate <- function(state, newfile, flds=NULL, nIter.jumping, nIter.transiti
   for (i in seq_along(state$files)) {
     f <- state$files[[i]]
     flds1 <- state$cmpdetails$flds[[i]]
-    thiscmp <- BRL::compareRecords(
+    thiscmp <- compareRecords(
       df1 = f, df2 = newfile, flds = state$cmpdetails$fldsglobal,
-      flds1 = flds1, flds2 = flds2, types = state$cmpdetails$types,
+      flds1 = flds1, flds2 = flds, types = state$cmpdetails$types,
       breaks = state$cmpdetails$breaks
     )
-    newcmps <- c(newcmp, list(thiscmp))
+    newcmps <- c(newcmps, list(thiscmp))
   }
   cmpdata <- c(state$comparisons, list(newcmps))
   # Append the file to the list of files
@@ -27,16 +27,93 @@ SMCMCupdate <- function(state, newfile, flds=NULL, nIter.jumping, nIter.transiti
   cmpdetails <- state$cmpdetails
   cmpdetails$flds <- c(cmpdetails$flds, list(flds))
 
+  # Groupings for m and u
+  nDisagLevs <- cmpdata[[1]][[1]]$nDisagLevs
 
-  # TODO: the actual SMCMC update
+  # Initialize streaming link object. Unlinked to new file, randomly chosen
+  # value from existing state
+  filesizes <- rep(0, length(files))
+  for (i in seq_along(files)) {
+    filesizes[i] <- nrow(files[[i]])
+  }
 
+  # How big is the ensemble we are working with?
+  ensemblesize <- ncol(state$Z)
+
+  # Create cluster if necessary and register for parallel execution
+  if (cores > 1) {
+    cl <- parallel::makeCluster(cores)
+    doParallel::registerDoParallel(cl)
+    if (!is.null(seed)) {
+      message("Random seed not set in parallel execution.")
+    }
+  } else {
+    foreach::registerDoSEQ()
+    if (!is.null(seed)) {
+      set.seed(seed)
+    }
+  }
+  `%dopar%` <- foreach::`%dopar%`
+
+  # Now do the SMCMC update on each member of the ensemble
+  samplist <- foreach::foreach(s=seq_len(ensemblesize), .inorder=TRUE) %dopar% {
+    # Initial values
+    mcurr <- state$m[,s]
+    ucurr <- state$u[,s]
+    slcurr <- streaminglinks(filesizes)
+    slcurr <- swapprefix(slcurr, state$Z[,s], conflict="error")
+
+    # Jumping kernel for links to new file
+    for (i in seq_len(nIter.jumping)) {
+      if (proposals.jumping == "LB") {
+        slcurr <- draw.Z.locbal.lastfile(newcmps, slcurr, mcurr, ucurr,
+                                         state$priors$aBM, state$priors$bBM,
+                                         blocksize=blocksize)
+      } else if (proposals.jumping == "component") {
+        # TODO: componentwise jumping kernel proposals
+      }
+    }
+
+    # Transition kernel for all parameters
+    for (i in seq_len(nIter.transition)) {
+      # m and u full conditional update
+      tmp <- r_m_u_fc_smcmc(cmpdata, slcurr, state$priors$a, state$priors$b)
+      mcurr <- tmp$m
+      ucurr <- tmp$u
+
+      for (f in seq(2, length(files))) {
+        if (proposals.jumping == "LB") {
+          # TODO: locally balanced transition kernel proposals
+        } else if (proposals.jumping == "component") {
+          # TODO: componentwise transition kernel proposals
+        }
+      }
+    }
+
+    # Return a list of the ending values
+    list(m=mcurr, u=ucurr, sl=slcurr)
+  }
+  if (cores > 1) {
+    parallel::stopCluster(cl)
+  }
+
+  # Pack parallel results into arrays
+  msamples <- matrix(NA, nrow=nrow(state$m), ncol=ensemblesize)
+  usamples <- matrix(NA, nrow=nrow(state$u), ncol=ensemblesize)
+  Zsamples <- matrix(NA, nrow=length(savestate(samplist[[1]]$sl)), ncol=ensemblesize)
+  for (s in seq_len(ensemblesize)) {
+    msamples[,s] <- samplist[[s]]$m
+    usamples[,s] <- samplist[[s]]$u
+    Zsamples[,s] <- savestate(samplist[[s]]$sl)
+  }
 
   # Construct and return the new link state
+  iterfilter <- setdiff(seq_len(nIter), seq_len(burn))
   structure(
     list(
-      Z = NULL,
-      m = NULL,
-      u = NULL,
+      Z = Zsamples[,iterfilter,drop=F],
+      m = msamples[,iterfilter,drop=F],
+      u = usamples[,iterfilter,drop=F],
       files = files,
       comparisons = cmpdata,
       priors = state$priors,
