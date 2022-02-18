49808fa7acd0a6617d37004b72755385f822f471
Ian Taylor <ian.taylor@colostate.edu>
2022-02-18 15:17:35 -0700

    Documentation on precision and recall.

--- R/postprocessing.R
+++ R/postprocessing.R
@@ -20,9 +20,12 @@
 #' @export
 thinsamples <- function(state, count) {
   N <- ncol(state$Z)
-  if (count > 1) {
+  if ((count > 1) && (count <= N)) {
     every <- (N - 1) %/% (count - 1)
     iterfilter <- seq(N - (count - 1) * every, N, by=every)
+  } else if (count > N) {
+    warning("Cannot filter to greater number of samples")
+    iterfilter <- seq_len(N)
   } else {
     iterfilter <- c(N)
   }
@@ -41,4 +44,59 @@ thinsamples <- function(state, count) {
     ),
     class = "bstrlstate"
   )
+}
+
+#' Extract the links from a bstrlstate object into a list of streaminglinks
+#' objects.
+#'
+#' @param state A bstrlstate object returned by one of the main RL functions.
+#'
+#' @return A list of streaminglinks objects, one per posterior sample contained
+#'   in 'state'.
+#'
+#' @export
+extractlinks <- function(state) {
+  `%do%` <- foreach::`%do%`
+  filesizes <- rep(NA, length(state$files))
+  for (f in seq_along(state$files)) {
+    filesizes[f] <- nrow(state$files[[f]])
+  }
+  foreach::foreach(i = seq_len(ncol(state$Z))) %do% {
+    streaminglinks(filesizes, state$Z[,i])
+  }
+}
+
+#' Calculate the precision of estimated links relative to true links
+#'
+#' @param sl.est streaminglinks object representing link estimates
+#' @param sl.true streaminglinks object representing true links
+#'
+#' @return The precision of the estimated links.
+#'
+#' @export
+precision <- function(sl.est, sl.true) {
+  estlinks <- alllinks(sl.est, idx="global")
+  istrue <- mapply(function(x, y, sl) islinked.gl(sl, x, y),
+                   estlinks$idx1, estlinks$idx2,
+                   MoreArgs = list(sl.true),
+                   SIMPLIFY = TRUE)
+  mean(istrue)
+}
+
+
+#' Calculate the recall of estimated links relative to true links
+#'
+#' @param sl.est streaminglinks object representing link estimates
+#' @param sl.true streaminglinks object representing true links
+#'
+#' @return The recall of the estimated links.
+#'
+#' @export
+recall <- function(sl.est, sl.true) {
+  truelinks <- alllinks(sl.true, idx="global")
+  isest <- mapply(function(x, y, sl) islinked.gl(sl, x, y),
+                   truelinks$idx1, truelinks$idx2,
+                   MoreArgs = list(sl.est),
+                   SIMPLIFY = TRUE)
+  mean(isest)
 }
